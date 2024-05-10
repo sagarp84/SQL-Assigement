da27563bf8dbbde29424147b58bd840a757bc1a0
sagarp84 <153709594+sagarp84@users.noreply.github.com>
2024-05-10 15:36:31 +0530

    Add files via upload

---
+++ SQL Assigement 1.sql
@@ -0,0 +1,40 @@
+---Display all information in the tables EMP and DEPT.----
+use hr_db
+select * from departments;
+select * from employees;
+---Display only the hire date and employee name for each employee. ---
+select  hire_date,first_name,last_name from employees;
+
+---Display the ename concatenated with the job ID, separated by a comma and space, and
+name the column Employee and Title ----
+select CONCAT(first_name,' ',last_name,',',job_id) as "Employee and Title"
+from employees;
+----Display the hire date, name and department number for all clerks. ---
+select hire_date,first_name,last_name,department_id from employees;
+---. Create a query to display all the data from the EMP table. Separate each column by a
+comma. Name the column THE OUTPUT ----
+select CONCAT(employee_id,',',first_name,',',last_name,',',email,',',phone_number,',',hire_date,',',job_id,',',salary,',',commission_pct,',',manager_id,',',department_id )as The_OutPut from employees;
+---. Display the names and salaries of all employees with a salary greater than 2000. ----
+select first_name,last_name,salary from employees
+where salary>2000;
+----Display the names and dates of employees with the column headers "Name" and "Start date"----
+select CONCAT(first_name,' ',last_name)as "NAME",hire_date as "Start_date" from employees;
+
+--- Display the names and hire dates of all employees in the order they were hired---
+select CONCAT(first_name,' ',last_name)as "NAME", hire_date from employees
+order by hire_date asc;
+----Display the names and salaries of all employees in reverse salary order----
+select CONCAT(first_name,' ',last_name)as "NAME",salary from employees
+order by salary desc;
+---Display 'ename" and "deptno" who are all earned commission and display salary in reverse order-----
+select CONCAT(first_name,' ',last_name)as "ename",department_id,commission_pct,salary from employees
+order by salary desc;
+---- Display the last name and job title of all employees who do not have a manager ---
+select last_name,job_id from employees
+where manager_id is null;
+---. Display the last name, job, and salary for all employees whose job is sales representative or stock clerk and whose salary is not equal to $2,500, $3,500, or $5,000
+or stock clerk and whose salary is not equal to $2,500, $3,500, or $5,000------select last_name,job_id as job ,salary from employeeswhere (job_id= 'SA_REP' or job_id='ST_CLERK')AND salary not in (2500,3500,5000);
+
+
+
+sele
---
+++ SQL Assigement 2.sql
@@ -0,0 +1,77 @@
+use hr_db
+select * from employees
+
+---1 Display the maximum, minimum and average salary and commission earned. ---
+select max(salary) as MAX_Salary,
+       min(salary) as MIN_Salary,
+	   avg(salary) as AVG_Salary,
+	   max(commission_pct) as MAX_Commission,
+	   min(commission_pct) as MIN_Commission,
+	   Avg(commission_pct) as AVG_Commission
+	   from employees;
+
+---2Display the department number, total salary payout and total commission payout for each department. ---
+select department_id,sum(salary) as Total_salary_payout,sum(commission_pct)as Total_commission_payout from employees
+group by department_id;
+
+---3 Display the department number and number of employees in each department. ---
+	 select department_id as deparment_number,count(employee_id) as Employees from employees
+	 group by department_id;
+
+----4Display the department number and total salary of employees in each department. ---
+ select department_id as deparment_numbe,sum(salary)as Total_salary from employees
+ group by department_id;
+
+ ----5Display the employee's name who doesn't earn a commission. Order the result set without using the column name ---
+select CONCAT(first_name,' ',last_name)as employees_name from employees
+where commission_pct is null
+order by 1;
+
+----6 Display the employees name, department id and commission. If an Employee doesn't
+earn the commission, then display as 'No commission'. Name the columns appropriately----select CONCAT(first_name,' ',last_name)as employees_name,department_id,ISNULL(null,'No Commission') as commission from employees;---7Display the employee's name, salary and commission multiplied by 2. If an Employee
+doesn't earn the commission, then display as 'No commission. Name the columns
+appropriately-----select CONCAT(first_name,' ',last_name)as employees_name,salary,(commission_pct*2),ISNULL(null,'No Commission') as commission from employees;----8Display the employee's name, department id who have the first name same as another employee in the same department----
+select employees.first_name,employees.last_name,departments.department_id
+from employees
+inner join departments
+on employees.department_id=departments.department_id;
+
+----9-Display the sum of salaries of the employees working under each Manager----
+select manager_id,sum(salary) as Total_salary from employees
+group by manager_id;
+
+----10-Select the Managers name, the count of employees working under and the department ID of the manager.----
+select manager_id,count(employee_id) as employee_working,department_id from employees
+group by manager_id,department_id;
+----11 Select the employee name, department id, and the salary. Group the result with the manager name and the employee last name should have second letter 'a! ----
+select CONCAT(first_name,' ',last_name)as employees_name,salary,department_id,manager_id as manager_name from employees
+where SUBSTRING(last_name,2,1)='a'
+order by manager_name;
+
+---12Display the average of sum of the salaries and group the result with the department id.Order the result with the department id. ---
+SELECT department_id,avg(sum_salary) As average_salary
+from(
+     select department_id,sum(salary) As sum_salary
+	 from employees
+	 group by department_id
+	 ) summed_salaries
+	 group by department_id
+	 order by department_id;
+
+---13-Select the maximum salary of each department along with the department id---
+SELECT department_id,max(salary) As max_salary
+from employees
+group by department_id;
+
+----14- Display the commission, if not null display 10% of salary, if null display a default value 1---
+select commission_pct,isnull(null,1) as commission,(salary*0.1)as adjust_commission from employees;
+
+
+
+
+
+
+
+
+
+
